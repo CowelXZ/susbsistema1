d466a68f14b54b17afb2073012505f5dea4c3e96
RAM518 <gomezram518@gmail.com>
2026-02-05 13:44:20 -0600

    primercomit

---
+++ js/mains.js
@@ -0,0 +1,5 @@
+"use strict";
+// src/prueba.ts
+const mensaje = "¡TypeScript está funcionando en Subsistema 1!";
+console.log(mensaje);
+alert(mensaje);
---
+++ src/mains.ts
@@ -0,0 +1,4 @@
+
+const mensaje: string = "¡TypeScript está funcionando en Subsistema 1!";
+console.log(mensaje);
+alert(mensaje);
